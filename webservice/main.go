9d6d5292544e211f92816bcf22ffb36621a14306
Dmitriy Strelnikov <dm.strelnikov@yandex.ru>
2020-11-19 09:58:44 +0300

    add module with struct and vars

--- webservice/main.go
+++ webservice/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/dm0610/Golang/webservice/models"
 )
 
 func main() {
-	fmt.Println("Hello from a module webservice, Gophers!")
+	//fmt.Println("Hello from a module webservice, Gophers!")
+	u := models.User{
+		ID:        2,
+		FirstName: "Dmitriy",
+		LastName:  "Strelnikov",
+	}
+	fmt.Println(u)
 }
