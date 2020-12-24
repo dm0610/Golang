ef0f6ecd4e204baff4cf2ea3e732a0d948ced8cc
Dmitriy Strelnikov <dm.strelnikov@yandex.ru>
2020-12-24 22:45:26 +0300

    upd1

--- webservice/main.go
+++ webservice/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/dm0610/Golang/webservice/models"
+	"github.com/dm0610/Golang/webservice/controllers"
 )
 
 func main() {
 	//fmt.Println("Hello from a module webservice, Gophers!")
-	u := models.User{
-		ID:        2,
-		FirstName: "Dmitriy",
-		LastName:  "Strelnikov",
-	}
-	fmt.Println(u)
+	/*	u := models.User{
+			ID:        2,
+			FirstName: "Dmitriy",
+			LastName:  "Strelnikov",
+		}
+		fmt.Println(u)
+	*/
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
